263a3d604df2e979c1e8671b9956e601aa8c585a
C-Probert <steven.chris.probert@gmail.com>
2015-04-11 18:15:16 -0600

    Added controller and filter logic

--- controller.js
+++ controller.js
@@ -1,92 +1,73 @@
 var tableApp = angular.module('tableApp',[]);
 
+/*
+ * Table data model is represented in a 2d array of json objects.
+ * The object is a cell's value and multiplier. 	
+ *
+ * */
 tableApp.controller('tableController', function($scope){
   var array = [];
-  //$scope.data.push(json)
-    var json = 
-  [{'multiplier':1, 'value':undefined},
-  {'multiplier':2, 'value':undefined},
-  {'multiplier':3, 'value':undefined},
-  {'multiplier':4, 'value':undefined},
-  {'multiplier':5, 'value':undefined},
-  {'multiplier':6, 'value':undefined}];
 
-  array.push(json);
-  $scope.data = array;
+  //default model 
+  var json = 
+  [{'multiplier':1, 'value':0},
+  {'multiplier':2, 'value':0},
+  {'multiplier':3, 'value':0},
+  {'multiplier':4, 'value':0},
+  {'multiplier':5, 'value':0},
+  {'multiplier':6, 'value':0}];
 
-$scope.cellFocus = function(index){
+array.push(json);
+$scope.data = array;
+
+//event wiring. When cell text changes, check row index. If last row, append new model to data.
+$scope.cellEdit = function(index){
 
   if(index == $scope.data.length -1){
 
     var json = 
-  [{'multiplier':1, 'value':undefined},
-  {'multiplier':2, 'value':undefined},
-  {'multiplier':3, 'value':undefined},
-  {'multiplier':4, 'value':undefined},
-  {'multiplier':5, 'value':undefined},
-  {'multiplier':6, 'value':undefined}];
+      [{'multiplier':1, 'value':0},
+    {'multiplier':2, 'value':0},
+    {'multiplier':3, 'value':0},
+    {'multiplier':4, 'value':0},
+    {'multiplier':5, 'value':0},
+    {'multiplier':6, 'value':0}];
     $scope.data.push(json);
   }
 
 }
 
-
-
 });
 
+
+/*filter that totals rows and columns.
+ * the direction param signifies which direction (row or column) to traverse the 2d array. 
+ * the index marks which row/column the total is being applied to.
+ */
 tableApp.filter('total', function(){
 
   return function(data, index, direction){
 
 
     var total = 0;
-    //totalByOrientation = function(cellTotal){
-
 
     if(direction){
       var length = data[index].length;
       for(var i = 0; i < length; i++){
-	
+
 	var item = data[index][i];
-	if(item.value != undefined){
-	  total += (item.multiplier * item.value);
-	}
+	total += (item.multiplier * item.value);
       }
     }else{
       var length = data.length;
       for(var i = 0; i < length; i++){
-	var item = data[i][index];
-	if(item.value != undefined){
-	  total += item.value;
-	}
 
-	
+	var item = data[i][index];
+	total += (item.multiplier * item.value);
       }
 
     }
-    
-    //var i = data.length;
-    /*
-    while(i--){
-      var row = data[i];
-      var j = row.length;
-      while(j--){
-	var col = row[j];
-	//total += (col[i].multiplier * col[i].value);
-      }
-    }*/
-    
 
     return total;
   }
-
-  });
-
-
-
-var json = [{'multiplier':1, 'value':undefined},
-    {'multiplier':2, 'value':undefined},
-    {'multiplier':3, 'value':undefined},
-    {'multiplier':4, 'value':undefined},
-    {'multiplier':5, 'value':undefined},
-    {'multiplier':6, 'value':undefined}];
+});
